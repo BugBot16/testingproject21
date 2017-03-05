ad9a79b337ee37cc67feefb8fb4d42eff54af9b8
testingaccount21 <mcdonagh.sinead95@gmail.com>
2017-03-05 18:57:32 +0000

    Add files via upload

---
+++ testing.c
@@ -0,0 +1,16 @@
+
+
+int main(){
+
+double d=1.0/2
+    
+    int i=3;
+    int j=4;
+    
+if(i==j)
+      printf("hello world!");
+      
+i=i+1; 
+  returni;
+      
+      }
---
+++ testingCPP.cpp
@@ -0,0 +1,14 @@
+int main(){
+
+double d=1.0/2
+    
+    int i=3;
+    int j=4;
+    
+if(3==4)
+      printf("hello world!");
+      
+i=i+1; 
+  returni;
+      
+      }
